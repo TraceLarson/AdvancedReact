ab019282f854ed8c3c58b3de3abedfe6ace1b467
Trace Larson <trace.larson4917@gmail.com>
2019-04-05 14:33:17 -0500

    created second toggle component to inlcude render props, imported Toggle from ToggleRenderProps component in App.js

--- advanced-react/src/App.js
+++ advanced-react/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Toggle from './Toggle';
+import Toggle from './ToggleRenderProps';
 
 class App extends Component {
   state = {};
