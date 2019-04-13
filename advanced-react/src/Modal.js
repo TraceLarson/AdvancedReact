4b9bf16ae1233b7aef4c84e6fd261832063e161f
Trace Larson <trace.larson4917@gmail.com>
2019-04-13 15:20:43 -0500

    created close svg icon that takes in props and lives in Utilities directory. Icon.js can hold multiple icons available through switch statement and props

--- advanced-react/src/Modal.js
+++ advanced-react/src/Modal.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 import Portal from './Portal';
+import Icon from './Utilities/Icon';
 
 export default class Modal extends Component {
   state = {};
@@ -13,7 +14,9 @@ export default class Modal extends Component {
         {on && (
           <ModalWrapper>
             <ModalCard>
-              <CloseButton onClick={toggle}>CLOSE</CloseButton>
+              <CloseButton onClick={toggle}>
+                <Icon name="close" color="blue" />
+              </CloseButton>
               <div>{children}</div>
             </ModalCard>
             <Background onClick={toggle} />
@@ -50,6 +53,9 @@ const CloseButton = styled.button`
   position: absolute;
   top: 0;
   right: 0;
+  border: none;
+  background: transparent;
+  padding: 10px;
 `;
 
 const Background = styled.div`
