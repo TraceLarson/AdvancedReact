9fb8602ec69f711f92dee5b495b6213de3138f75
Trace Larson <trace.larson4917@gmail.com>
2019-04-14 16:30:05 -0500

    Imported createContext, created const UserContext = createContext(), created UserContext.Provider with this.props.children inside and value prop of {this.state}

--- advanced-react/src/App.js
+++ advanced-react/src/App.js
@@ -1,9 +1,36 @@
-import React, { Component, Fragment } from 'react';
+/* eslint-disable react/no-multi-comp */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/no-unused-state */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-unused-vars */
+
+import React, { Component, Fragment, createContext } from 'react';
 import './App.css';
 
 import { Toggle } from 'Utilities';
 import { Modal } from 'Elements';
 import logo from './logo.svg';
+
+const UserContext = createContext();
+
+class UserProvider extends Component {
+  state = {
+    id: '123',
+    name: 'Trace',
+    email: 'trace@email.com',
+  };
+
+  return() {
+    <UserContext.Provider
+      value={{
+        user: this.state,
+      }}
+    >
+      {this.props.children}
+    </UserContext.Provider>;
+  }
+}
 
 class App extends Component {
   state = {};
